3bc682162c45afa5a906264838bb0447d25fce75
aabboudi <139150604+aabboudi@users.noreply.github.com>
2025-01-07 13:55:03 +0100

    Rectified extra credit endpoint logic

--- app/api/api_v1.py
+++ app/api/api_v1.py
@@ -31,15 +31,17 @@ async def get_scoreboard():
 Admin Access
 '''
 # Add player score
-@router.put("/add-extra-credit/{nccid}", tags=["Admin"])
+@router.put("/add-extra-credit", tags=["Admin"])
 # async def update_score(nccid: str, score: int, password: str = Depends(verify_admin_password)):
 async def add_points(nccid: str, score: int):
   try:
     # TODO: Review score update and extra credit list
     players_collection.find_one_and_update(
       {"nccid": nccid},
-      {"$inc": {"score": score}},
-      {"$set": {"updatedAt": datetime.now()}},
+      {
+        "$inc": {"score": score},
+        "$set": {"updatedAt": datetime.now()}
+      },
       return_document=True
     )
     return {"message": "Score updated successfully", "nccid": nccid, "score": score}
